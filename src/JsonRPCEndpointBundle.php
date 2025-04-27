4e498c0a04889d93588ddd74f25ba44d694f6fc5
github-ci <you@example.com>
2025-04-27 16:54:21 +0000

    忽略无用调用堆栈+

--- src/JsonRPCEndpointBundle.php
+++ src/JsonRPCEndpointBundle.php
@@ -4,7 +4,9 @@ namespace Tourze\JsonRPCEndpointBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BacktraceHelper\Backtrace;
+use Tourze\JsonRPC\Core\Procedure\BaseProcedure;
 use Tourze\JsonRPCEndpointBundle\Serialization\JsonRpcCallDenormalizer;
+use Tourze\JsonRPCEndpointBundle\Service\JsonRpcEndpoint;
 use Tourze\JsonRPCEndpointBundle\Service\JsonRpcRequestHandler;
 
 class JsonRPCEndpointBundle extends Bundle
@@ -12,7 +14,9 @@ class JsonRPCEndpointBundle extends Bundle
     public function boot(): void
     {
         parent::boot();
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(BaseProcedure::class))->getFileName());
         Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcRequestHandler::class))->getFileName());
         Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcCallDenormalizer::class))->getFileName());
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(JsonRpcEndpoint::class))->getFileName());
     }
 }
